a5a104e9b7edcb4426904b575ae9a26f856f0b7b
JunSeo <oponize@naver.com>
2021-10-25 22:38:30 +0900

    learn tensorflow.js

--- React_testing/simple_test/src/App.tsx
+++ React_testing/simple_test/src/App.tsx
@@ -1,38 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [disable, setDisable] = useState(false);
-  const onIncrease = () => {
-    setCount((prev) => prev + 1);
-  };
-  const onDecrease = () => {
-    setCount((prev) => prev - 1);
-  };
-  const onDisable = () => {
-    setDisable((prev) => !prev);
-  };
   return (
-    <>
-      <div className="container">
-        <h1 data-testid="counter">{count}</h1>
-        <button disabled={disable} data-testid="increase" onClick={onIncrease}>
-          +
-        </button>
-        <button disabled={disable} data-testid="decrease" onClick={onDecrease}>
-          -
-        </button>
-        <div>
-          <button
-            onClick={onDisable}
-            data-testid="disable"
-            style={{ backgroundColor: "blue" }}
-          >
-            on/off
-          </button>
-        </div>
-      </div>
-    </>
+    <div>
+      <h1>asdasd...asdas..</h1>
+    </div>
   );
 };
 
---
+++ tensorflow.js/myModels/src/main.mjs
@@ -0,0 +1,3 @@
+import tf from "../node_modules/@tensorflow/tfjs";
+
+console.log(tf);
